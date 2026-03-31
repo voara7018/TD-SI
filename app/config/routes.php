7a65de2d1a02c2a0ce09c769ce969ab8218f96a2
Lofo-hub <lofoniaina1325@gmail.com>
2026-03-31 15:06:46 +0300

    yes

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,5 +2,8 @@
 
 require_once __DIR__ . '/../repositories/ProduitRepositories.php';
 require_once __DIR__ . '/../controllers/ProduitController.php';
+require_once __DIR__ . '/../controllers/CaisseController.php';
+require_once __DIR__ . '/../controllers/CaisseRepositories.php';
 
 Flight::route('GET /', ['ProduitController', 'afficherListeProduits']);
+Flight::route('GET /caisses', ['CaisseController', 'afficherListeCaisse']);
